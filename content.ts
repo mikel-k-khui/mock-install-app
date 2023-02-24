663b60d314a0b28b391dbf57b29771cfeafebf1a
Mike <michael@okos.ca>
2023-02-24 21:37:39 +0000

    add code in files types: .ts,.tsx remove code in files types: .ts,.tsx

--- content.ts
+++ content.ts
@@ -1,4 +1,4 @@
 console.log("cjgey")
 console.log("gwngr")
 console.log("miodm")
-console.log("tkomj")
+console.log("ffkel")
--- content.tsx
+++ content.tsx
@@ -1,3 +1,3 @@
 console.log("sbnfk")
 console.log("lxdhe")
-console.log("csljt")
+console.log("nbgbi")
